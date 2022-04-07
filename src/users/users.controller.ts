38312e6ddee7dfceeb9c564dfdc03e0eea61a687
DESKTOP-B421EOF\admin <deepak.bawa@orionesolutions.in>
2022-04-07 18:22:46 +0530

    removed console log

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -18,7 +18,6 @@ export class UsersController {
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     try {
-      console.log('createUserDto', createUserDto);
       return await this.usersService.create(createUserDto);
     } catch (ex) {
       throw ex;
